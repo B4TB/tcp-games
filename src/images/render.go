cc716be39956822f57236963bb4a58ac50dcd834
Tali Auster <me@tali.network>
2024-09-29 12:08:10 -0700

    feat: add bw and color to images

--- src/images/render.go
+++ src/images/render.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"io"
 	"net"
+	"fmt"
 	_ "image/png"
 	_ "image/jpeg"
 	_ "golang.org/x/image/webp"
@@ -15,10 +16,37 @@ import (
 
 var chars = []rune{' ', '░', '▒', '▓'}
 
-func compress(img image.Image) string {
-	var ret string
-	var r, g, b, k uint32
+type ascii_fn func(image.Image, int, int) string
+
+func pix_to_bw(img image.Image, x, y int) string {
+	var r, g, b uint32
 	var lightness float64
+
+	r, g, b, _ = img.At(x, y).RGBA()
+
+	lightness = 0.2126 * float64(r) / float64(0xffff) + 0.7152 * (float64(g) / float64(0xffff)) + 0.0722 * (float64(b) / float64(0xffff))
+
+	k := max(int32(lightness * 4) - 1, 0)
+	return string(chars[k])
+}
+
+func pix_to_rgb(img image.Image, x, y int) string {
+	var r, g, b uint32
+
+	r, g, b, _ = img.At(x, y).RGBA()
+	rf := float64(r) / float64(0xffff)
+	gf := float64(g) / float64(0xffff)
+	bf := float64(b) / float64(0xffff)
+
+	rs := int(rf * 255)
+	gs := int(gf * 255)
+	bs := int(bf * 255)
+
+	return fmt.Sprintf("\033[38;2;%d;%d;%dm█", rs, gs, bs)
+}
+
+func compress(img image.Image, converter ascii_fn) string {
+	var ret string
 
 	target_width := 100
 	width := img.Bounds().Max.X - img.Bounds().Min.X
@@ -31,29 +59,32 @@ func compress(img image.Image) string {
 
 	for y := range(target_height) {
 		for x := range 100 {
-			r, g, b, _ = img.At(x * xstride, y * ystride).RGBA()
-
-			lightness = 0.2126 * float64(r) / float64(0xffff) + 0.7152 * (float64(g) / float64(0xffff)) + 0.0722 * (float64(b) / float64(0xffffff))
-
-			k = uint32(lightness * 4)
-
-			ret += string(chars[k])
+			ret += converter(img, x * xstride, y * ystride)
 		}
-		ret += "\n"
+		ret += "\033[0m\n"
 	}
 
 	return ret
 }
 
-func make_image(r io.Reader) (string, error) {
+func make_image(r io.Reader, converter *ascii_fn) (string, error) {
 	reader := bufio.NewReader(r)
-	url, err := reader.ReadString('\n')
+	line, err := reader.ReadString('\n')
 	if err != nil {
 		log.Printf("err: %w", err)
 		return "fucky wucky\n", err
 	}
 
-	resp, err := http.Get(strings.TrimSpace(url))
+	line = strings.TrimSpace(line)
+	if line == "color" {
+		*converter = pix_to_rgb
+		return "Using RGB.\n", nil
+	} else if line == "bw" {
+		*converter = pix_to_bw
+		return "Using BW.\n", nil
+	}
+
+	resp, err := http.Get(line)
 	if err != nil {
 		log.Printf("err: %w", err)
 		return "other fucky wucky\n", err
@@ -65,18 +96,23 @@ func make_image(r io.Reader) (string, error) {
 		return "fucky wucky!\n", err
 	}
 
-	return compress(img), nil
+	return compress(img, *converter), nil
 }
 
 func handleConn(conn net.Conn) {
+	var converter ascii_fn
+	converter = pix_to_rgb
+
+	conn.Write([]byte("Welcome! Paste an image URL to view. Commands 'color' and 'bw' can be used to alter the output.\n"))
+
 	for {
-		img, err := make_image(conn)
+		img, err := make_image(conn, &converter)
+		conn.Write([]byte(img))
+
 		if err != nil {
 			log.Printf("%w", err)
 			break
 		}
-
-		conn.Write([]byte(img))
 	}
 }
 
