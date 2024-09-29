627eccebe4c8a26a4a09f20270083d2fa57dfa15
Tali Auster <me@tali.network>
2024-09-29 08:03:15 -0700

    feat: init flake and package nim.py

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A variety of games playable via netcat/telnet.";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        version = "0.1.0";
+      in {
+        packages = {
+          nim = pkgs.stdenv.mkDerivation {
+            pname = "nim";
+            inherit version;
+
+            dontBuild = true;
+            dontUnpack = true;
+
+            propagatedBuildInputs = with pkgs; [ python3 ];
+            installPhase = ''
+              install -Dm755 ${./examples/nim.py} $out/bin/nim
+            '';
+          };
+        };
+      });
+}
